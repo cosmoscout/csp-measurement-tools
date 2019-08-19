d8c95bef5b76a2bbd9431db7f8733d50341328b9
Jonas Gilg <jonas.gilg@dlr.de>
2019-08-19 15:10:50 +0200

    :hammer: UI now works with Bootstrap instead of Materialize.

--- gui/js/measurement-tools-tab.js
+++ gui/js/measurement-tools-tab.js
@@ -2,16 +2,16 @@ init_slider("set_widget_scale", 0.2, 2.0, 0.01, [0.8]);
 
 function add_measurement_tool(name, icon) {
     const area = $('#measurement-tools');
-    area.append('<div class=\'col s4 center\'>\
-                    <label>\
-                        <input id=\'set_tool_' +
-        icon + '\' type=\'checkbox\' name=\'measurement-tool\' class=\'radio-button\' />\
-                        <div class=\'waves-effect waves-light block btn btn-large glass\'>\
-                            <i class=\'material-icons\'>' +
-        icon + '</i> ' + name + '\
-                        </div>\
-                    </label>\
-                </div>');
+    area.append(`<div class='col-4 center'>
+                    <label style="width: 100%; height: 100%">
+                        <input id='set_tool_${icon}' type='checkbox' name='measurement-tool' class='radio-button' />
+                        <div class='ripple block btn glass'>
+                            <i style="font-size: 80px" class='material-icons'>${icon}</i> 
+                            <br> 
+                            <span>${name}</span>
+                        </div>
+                    </label>
+                </div>`);
 
     $('#set_tool_' + icon).change(function () {
         $('#measurement-tools .radio-button').not('#set_tool_' + icon).prop('checked', false);
