4fb6f0086abbc8dc76fa9f642346959430ed9ecd
Hannes Kruse <Hannes.Kruse@dlr.de>
2019-12-13 16:02:06 +0100

    :construction: Gui refactoring

--- gui/js/measurement-tools-tab.js
+++ gui/js/measurement-tools-tab.js
@@ -1,29 +1,9 @@
 init_slider("set_widget_scale", 0.2, 2.0, 0.01, [0.6]);
 
 function add_measurement_tool(name, icon) {
-    const area = $('#measurement-tools');
-    area.append(`<div class='col-4 center' style='padding: 0 5px'>
-                    <label style="width: 100%; height: 100%">
-                        <input id='set_tool_${icon}' type='checkbox' name='measurement-tool' class='radio-button' />
-                        <div class='block btn glass'>
-                            <i style="font-size: 60px" class='material-icons'>${icon}</i> 
-                            <br> 
-                            <span>${name}</span>
-                        </div>
-                    </label>
-                </div>`);
-
-    $('#set_tool_' + icon).change(function () {
-        $('#measurement-tools .radio-button').not('#set_tool_' + icon).prop('checked', false);
-
-        if (this.checked) {
-            window.call_native('set_measurement_tool', name);
-        } else {
-            window.call_native('set_measurement_tool', 'none');
-        }
-    });
+    return CosmoScout.call('sidebar', 'addMeasurementTool', name, icon);
 }
 
 function deselect_measurement_tool() {
-    $('#measurement-tools .radio-button').prop('checked', false);
+    return CosmoScout.call('sidebar', 'deselectMeasurementTool');
 }
